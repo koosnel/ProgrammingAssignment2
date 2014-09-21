54ae8118f1e97e744518ead24c925d790a0f72ed
Cobus Nel <cobus@nel.org.za>
2014-09-21 20:22:03 +0200

    Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## 
+## Programming Assignment 2
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
 
